0d48cbafd1fd04f0b489f171105b0b76592405e8
awanmbandi <awanmbandi@gmail.com>
2024-09-07 00:46:37 -0400

    updated project configurations, added data fetch and subnet group

--- startup-script/app-upgrade.sh
+++ startup-script/app-upgrade.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Upgrade PHP version to 8
-sudo su
-apt update
-apt install -y php8.0
-apt install -y php8.1
-apt install -y php8.0-common php8.0-mysql php8.0-xml php8.0-gd php8.0-mbstring php8.0-curl
-apt install -y php8.1-common php8.1-mysql php8.1-xml php8.1-gd php8.1-mbstring php8.1-curl
-a2dismod php7.0
-a2enmod php8.0
-systemctl restart apache2
-php -v
-systemctl restart apache2
+sudo apt update
+sudo apt install -y php8.0
+sudo apt install -y php8.1
+sudo apt install -y php8.0-common php8.0-mysql php8.0-xml php8.0-gd php8.0-mbstring php8.0-curl
+sudo apt install -y php8.1-common php8.1-mysql php8.1-xml php8.1-gd php8.1-mbstring php8.1-curl
+sudo a2dismod php7.0
+sudo a2enmod php8.0
+sudo systemctl restart apache2
+sudo php -v
+sudo systemctl restart apache2
 
